b7b783f3964a549e04184539cf737387614b0ae3
pam-param <tamarova.irina@gmail.com>
2020-10-02 14:31:51 +0300

    feat: add solutions for count cats, carbon dating, dream team and what season tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let firstLetters = members.reduce((accumulator, member) => {
+    if (typeof(member) === 'string' && member.length > 0) {
+      accumulator.push(member.trim()[0].toUpperCase());
+    }
+    return accumulator;
+  }, []);
+
+  return firstLetters.sort().join('');
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date == undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  let toString = Object.prototype.toString;
+
+  if (toString.call(date) != toString.call(new Date())) {
+    throw new Error('Argument is not instance of Date !');
+  }
+
+  const winter = 'winter';
+  const spring = 'spring';
+  const summer = 'summer';
+  const fall = 'fall';
+
+  const seasons = [winter, winter,
+    spring, spring, spring, 
+    summer, summer, summer,
+    fall, fall, fall,
+    winter
+  ];
+
+  const month = date.getMonth();
+  if (seasons[month] === undefined) {
+    throw new Error('Unexpected month value!');
+  } else {
+    return seasons[month];
+  }
+
 };
