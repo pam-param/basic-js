6d105dac72c16bc633e0ff5228adee4debeb6837
pam-param <tamarova.irina@gmail.com>
2020-10-04 22:20:42 +0300

    feat: add solutions for hanoi tower and transform array tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
+  let solution = {};
+  solution.turns = 2 ** disksNumber - 1;
+  solution.seconds = Math.floor(3600 * solution.turns / turnsSpeed);
+
+  return solution;
 };
+
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Argument is not Array');
+  }
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+
+  let transformedArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === discardNext || arr[i] === discardPrev || arr[i] === doubleNext || arr[i] === doublePrev) {
+      continue;
+    }
+
+    if (arr[i - 1] === discardNext) {
+      continue;
+    }
+
+    if (arr[i - 1] === doubleNext) {
+      transformedArray.push(arr[i]);
+    }
+
+    transformedArray.push(arr[i]);
+
+    if(arr[i + 1] === doublePrev) {
+      transformedArray.push(arr[i]);
+    }
+
+    if(arr[i + 1] === discardPrev) {
+      transformedArray.pop();
+    }
+  }
+
+  return transformedArray;
+
 };
+
